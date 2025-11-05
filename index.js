0ed7296ccbba7ebf05b30df94355417f80ae31b8
josemmp2005 <josemayenp@gmail.com>
2025-11-05 19:41:17 +0100

    fix: remove trailing slash from Netlify CORS origin URL

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors({
-    origin: ["http://localhost:5173", "https://fitoprice.netlify.app/"],
+    origin: ["http://localhost:5173", "https://fitoprice.netlify.app"],
     credentials: true,
 }));
 
