c3475d0dbf2efa5b592ce71dfa290c7ee9bc61e6
josemmp2005 <josemayenp@gmail.com>
2025-11-06 17:04:11 +0100

    fix: remove duplicate last-scraped endpoint in product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -5,9 +5,9 @@ const router = express.Router();
 
 router.post("/import", importProducts);
 router.get("/all", getAllProducts);
+router.get("/last-scraped", getLastScrapedAt);
 router.get("/:id", getProductById);
 router.get("/:id/last-price", getLastProductPrice);
 router.get("/:id/price-history", getHistoryProductPrice);
-router.get("/last-scraped", getLastScrapedAt);
 
 export default router;
