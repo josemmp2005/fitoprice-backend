23ad70bf37422b297ec19569237532357aa6d104
josemmp2005 <josemayenp@gmail.com>
2025-11-06 17:54:30 +0100

    feat: add getfifteenProducts endpoint to product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { importProducts, getAllProducts, getProductById, getLastProductPrice, getHistoryProductPrice, getLastScrapedAt, getCountProducts } from "../controllers/productController.js";
+import { importProducts, getAllProducts, getfifteenProducts, getProductById, getLastProductPrice, getHistoryProductPrice, getLastScrapedAt, getCountProducts } from "../controllers/productController.js";
 
 const router = express.Router();
 
 router.post("/import", importProducts);
 router.get("/all", getAllProducts);
+router.get("/fifteen", getfifteenProducts);
 router.get("/last-scraped", getLastScrapedAt);
 router.get("/count", getCountProducts);
 router.get("/:id", getProductById);
