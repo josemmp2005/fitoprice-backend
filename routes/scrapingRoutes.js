6cc13327ae939af4213a10c25c9413938fe490cc
josemmp2005 <josemayenp@gmail.com>
2025-11-10 17:18:08 +0100

    feat: add POST route for creating new scraping jobs

--- routes/scrapingRoutes.js
+++ routes/scrapingRoutes.js
@@ -1,7 +1,10 @@
 import express from "express";
+import { createNewScrapingJob } from "../controllers/scrapingController.js";
+
 
 const router = express.Router();
 
+router.post("/scraping-job", createNewScrapingJob);
 
 
 export default router;
